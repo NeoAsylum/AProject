ecd7e6cca051322574640098ba1b4e70555db053
NeoAsylum <adrianplaaslink@gmx.de>
2022-07-15 17:50:08 +0200

    Swing Menu

--- EinmalMitProfis/src/execution/Main.java
+++ EinmalMitProfis/src/execution/Main.java
@@ -4,8 +4,17 @@ import java.awt.EventQueue;
 
 import gui.EinFrame;
 
+/**
+ * Class for executing program.
+ * @author Adrian
+ *
+ */
 public class Main {
   
+  /**
+   * Main method.
+   * @param args Parameters.
+   */
   public static void main(String[] args) {
       EventQueue.invokeLater(new Runnable() {
         public void run() {
